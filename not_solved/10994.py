b093d37df42eec20d0e560596bb452d8261256e6
wnsgud0428 <wnsgud0428@naver.com>
2022-04-15 17:35:47 +0900

    10994 not yet solved

---
+++ not_solved/10994.py
@@ -0,0 +1,16 @@
+N = int(input())
+stars = [[0 for i in range(4*(N-1)+1)] for i in range(4*(N-1)+1)]
+
+for i in range(N): # 0,1,2,3...
+    for j in range(4):
+        if j == 0: #위
+            for k in range()
+            stars[i*2][]
+            pass
+        elif j ==1: #왼쪽
+            pass
+        elif j ==2: #오른쪽
+            pass        
+        elif j ==3: #아래
+            pass        
+        
